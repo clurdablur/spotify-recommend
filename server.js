b58691dba278bb4ddd7004cce1ebb61657af13ac
clurdablur <coucc15@gmail.com>
2016-10-26 00:22:32 +0000

    fixed bugs

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var getFromApi = function(endpoint, args) {
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
            .end(function(response) {
-               emitter.emit('end', response.body);
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
             });
     return emitter;
 };
@@ -16,30 +21,21 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/search/:name', function(req, res) {
-    var artist;
-
-    var onSearchEnd = function(item) {
-        artist = item.artists.items[0];
-    	var relatedReq = getFromApi('artists/' + artist.id + '/related-artists');
-    	relatedReq.on('end', function addRelated (artistArray) {
-    		artist.related = artistArray.artists;
-        	res.json(artist);
-    	});
-    };
-
-    var onError = function() {
-        res.sendStatus(404);
-    };
-
     var searchReq = getFromApi('search', {
         q: req.params.name,
         limit: 1,
         type: 'artist'
     });
 
-    
-    searchReq.on('end', onSearchEnd);
-    searchReq.on('error', onError);
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        res.json(artist);
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
 });
 
-app.listen(8080);
+
+app.listen(process.env.PORT || 8080);
